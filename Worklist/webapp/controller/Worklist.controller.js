e99b4dd7b093f26d4a8d6d13c471098eb82148a7
Alexey <alleshkvch@gmail.com>
2024-10-23 20:21:49 +0300

    создание таблицы через js

--- Worklist/webapp/controller/Worklist.controller.js
+++ Worklist/webapp/controller/Worklist.controller.js
@@ -4,35 +4,23 @@ sap.ui.define([
 		"sap/ui/model/json/JSONModel",
 		"zjblessons/Worklist/model/formatter",
 		"sap/ui/model/Filter",
-		"sap/ui/model/FilterOperator"
-	], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+		"sap/ui/model/FilterOperator",
+		"sap/ui/core/Fragment" //добавлено
+	], function (BaseController, JSONModel, formatter, Filter, FilterOperator, Fragment) {
 		"use strict";
 
 		return BaseController.extend("zjblessons.Worklist.controller.Worklist", {
 
 			formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
-
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
+			
 			onInit : function () {
 				var oViewModel,
 					iOriginalBusyDelay,
 					oTable = this.byId("table");
-
-				// Put down worklist table's original value for busy indicator delay,
-				// so it can be restored later on. Busy handling on the table is
-				// taken care of by the table itself.
 				iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
-				// keeps the search state
 				this._aTableSearchState = [];
 
-				// Model used to manipulate control states
 				oViewModel = new JSONModel({
 					worklistTableTitle : this.getResourceBundle().getText("worklistTableTitle"),
 					shareOnJamTitle: this.getResourceBundle().getText("worklistTitle"),
@@ -43,11 +31,7 @@ sap.ui.define([
 				});
 				this.setModel(oViewModel, "worklistView");
 
-				// Make sure, busy indication is showing immediately so there is no
-				// break after the busy indication for loading the view's meta data is
-				// ended (see promise 'oWhenMetadataIsLoaded' in AppController)
 				oTable.attachEventOnce("updateFinished", function(){
-					// Restore original busy indicator delay for worklist's table
 					oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
 				});
 			},
@@ -56,22 +40,11 @@ sap.ui.define([
 			/* event handlers                                              */
 			/* =========================================================== */
 
-			/**
-			 * Triggered by the table's 'updateFinished' event: after new table
-			 * data is available, this handler method updates the table counter.
-			 * This should only happen if the update was successful, which is
-			 * why this handler is attached to 'updateFinished' and not to the
-			 * table's list binding's 'dataReceived' method.
-			 * @param {sap.ui.base.Event} oEvent the update finished event
-			 * @public
-			 */
+			
 			onUpdateFinished : function (oEvent) {
-				// update the worklist's object counter after the table update
 				var sTitle,
 					oTable = oEvent.getSource(),
 					iTotalItems = oEvent.getParameter("total");
-				// only update the counter if the length is final and
-				// the table is not empty
 				if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
 					sTitle = this.getResourceBundle().getText("worklistTableTitleCount", [iTotalItems]);
 				} else {
@@ -80,21 +53,11 @@ sap.ui.define([
 				this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
 			},
 
-			/**
-			 * Event handler when a table item gets pressed
-			 * @param {sap.ui.base.Event} oEvent the table selectionChange event
-			 * @public
-			 */
+			
 			onPress : function (oEvent) {
-				// The source is the list item that got pressed
 				this._showObject(oEvent.getSource());
 			},
 
-			/**
-			 * Event handler for navigating back.
-			 * We navigate back in the browser historz
-			 * @public
-			 */
 			onNavBack : function() {
 				history.go(-1);
 			},
@@ -102,10 +65,6 @@ sap.ui.define([
 
 			onSearch : function (oEvent) {
 				if (oEvent.getParameters().refreshButtonPressed) {
-					// Search field's 'refresh' button has been pressed.
-					// This is visible if you select any master list item.
-					// In this case no new search is triggered, we only
-					// refresh the list binding.
 					this.onRefresh();
 				} else {
 					var aTableSearchState = [];
@@ -118,48 +77,110 @@ sap.ui.define([
 				}
 
 			},
-
-			/**
-			 * Event handler for refresh event. Keeps filter, sort
-			 * and group settings and refreshes the list binding.
-			 * @public
-			 */
+			
 			onRefresh : function () {
 				var oTable = this.byId("table");
 				oTable.getBinding("items").refresh();
 			},
 
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
+			/*Вызов диалога для создания новой записи*/
+			onPressCreate : function () {
+				if(!this.pDialog){
+					this.pDialog = Fragment.load({
+						name: "WorkList1.view.CreateDialog"
+					});
+				}
+					this.pDialog.then(function(oDialog){
+						return oDialog.open();
+					});
+			},
+			/*==========================================================================================*/
+			/* inner methods*/
+			
 
-			/**
-			 * Shows the selected item on the object page
-			 * On phones a additional history entry is created
-			 * @param {sap.m.ObjectListItem} oItem selected Item
-			 * @private
-			 */
 			_showObject : function (oItem) {
 				this.getRouter().navTo("object", {
 					objectId: oItem.getBindingContext().getProperty("HeaderID")
 				});
 			},
 
-			/**
-			 * Internal helper method to apply both filter and search state together on the list binding
-			 * @param {sap.ui.model.Filter[]} aTableSearchState An array of filters for the search
-			 * @private
-			 */
 			_applySearch: function(aTableSearchState) {
 				var oTable = this.byId("table"),
 					oViewModel = this.getModel("worklistView");
 				oTable.getBinding("items").filter(aTableSearchState, "Application");
-				// changes the noDataText of the list in case there are no filter results
 				if (aTableSearchState.length !== 0) {
 					oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
 				}
-			}
+			},
 
+			//Задание структуры таблицы через js
+			_getTableTemplate : function () {
+				var oTable = this.byId('table');
+				 var oColumns = [
+						new sap.m.Column({
+						header : new sap.m.Label({
+							text:"DocumentNumber"
+						})
+					}),
+						new sap.m.Column({
+							header : new sap.m.Label({
+								text:"DocumentDate"
+							})
+						}),
+						new sap.m.Column({
+							header : new sap.m.Label({
+								text:"PlantText"
+							})
+						}),
+						new sap.m.Column({
+							header : new sap.m.Label({
+								text:"RegionText"
+							})
+						}),
+						new sap.m.Column({
+							header : new sap.m.Label({
+								text:"Description"
+							})
+						}),
+						new sap.m.Column({
+							header : new sap.m.Label({
+								text:"Created"
+							})
+						})
+				];
+				var oTemplate = new sap.m.ColumnListItem({
+					type: "Navigation",
+					navigated: true,
+					press: "onPress",  //при нажатии не работает
+					cells: [
+						new sap.m.Text ({
+							text: '{DocumentNumber}'
+						}),
+						new sap.m.Text ({
+							text: '{DocumentDate}'
+						}),
+						new sap.m.Text ({
+							text: '{PlantText}'
+						}),
+						new sap.m.Text ({
+							text: '{RegionText}'
+						}),
+						new sap.m.Text ({
+							text: '{Description}'
+						}),
+						new sap.m.Text ({
+							text: '{Created}'
+						})
+					]
+				});
+				
+				oColumns.map(function(column){
+					oTable.addColumn(column);
+					});
+					
+				oTable.bindItems('/zjblessons_base_Headers', oTemplate);
+			},
+			/*=============================================================================================*/
 		});
 	}
 );
